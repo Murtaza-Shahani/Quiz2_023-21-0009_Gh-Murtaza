d8178a677ae9d7c803e1c38513da0aaaa078d954
Murtaza <murtazashahani2021@gmail.com>
2023-09-25 00:36:45 -0700

    second commit

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -7,14 +7,21 @@ function calculateAverage(numbers) {
     else{
      avg = numbers/5 ;
     }
-  
-    
-  
-    return avg
+      return avg
   }
-  
-  
+
   const numbers = [1, 2, 3, 4, 5];
   const avg = calculateAverage(numbers);
   console.log(`The average is: ${avg}`);
+  
+
+  function ArrayWithIndex(array) {
+    array.map((value, index) => {
+      console.log(`Value at index ${index} is ${value}`);
+    });
+  }
+  
+  
+  const ARRAY = [10, 20, 30, 40, 50];
+  ArrayWithIndex(ARRAY);
   
