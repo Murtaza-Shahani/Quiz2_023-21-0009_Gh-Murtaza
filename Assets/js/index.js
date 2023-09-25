c225b2085306d08f11f268b4e248b2d98d9bda0d
Murtaza <murtazashahani2021@gmail.com>
2023-09-25 00:42:37 -0700

    third commit

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -25,3 +25,41 @@ function calculateAverage(numbers) {
   const ARRAY = [10, 20, 30, 40, 50];
   ArrayWithIndex(ARRAY);
   
+ 
+ 
+  const person = {
+    name: "Murtaza",
+    age: 21,
+    country: "pakistan",
+    semester: 5,
+    cms: "12345",
+    details: [
+      {
+        contactNumber: "555-555-5551",
+        email: "murtaza@example.com",
+      },
+      {
+        contactNumber: "555-555-5552",
+        email: "shahani@example.com",
+      },
+      {
+        contactNumber: "555-555-5553",
+        email: "ali@example.com",
+      },
+    ],
+  };
+  
+  // Printing values of the object:
+  console.log("Name:", person.name);
+  console.log("Age:", person.age);
+  console.log("Country:", person.country);
+  console.log("Semester:", person.semester);
+  console.log("CMS:", person.cms);
+  
+  console.log("Contact Details:");
+  person.details.forEach((detail, index) => {
+    console.log(`Contact ${index + 1}:`);
+    console.log("Contact Number:", detail.contactNumber);
+    console.log("Email:", detail.email);
+  });
+  
